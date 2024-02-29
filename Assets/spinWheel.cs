8e8390b3729b6918870ef15d479bde67eff2e6b9
AvinashGameDev-DL <117093224+GameDev-DL@users.noreply.github.com>
2024-02-29 18:25:01 +0530

    WordFamilyActivity WIP
    working on word family activity.

--- Assets/spinWheel.cs
+++ Assets/spinWheel.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class spinWheel : MonoBehaviour
 {
@@ -10,19 +12,34 @@ public class spinWheel : MonoBehaviour
     public AudioClip[] audios;
 
     public AnimationClip[] spinAnimation;
-
+    [SerializeField] private TextMeshProUGUI clickedText;
+    [SerializeField] private TextMeshProUGUI spinText;
+    public GameObject wordPanel;
     int count;
 
     public void Spin()
     {
-        if (count == 5)
-        {
-            Debug.Log("Game Over");
-            count = 0;
-        }
         StartCoroutine(playSpinAnimation());
-        count++;
+        count = Random.Range(0,5);
 
+    }
+
+    public void clickedTextSetter()
+    {
+        GameObject currentObject = EventSystem.current.currentSelectedGameObject;
+        clickedText.text = currentObject.name;
+        Debug.Log(currentObject.name);
+    }
+
+    private void WordPanelEnable()
+    {
+        wordPanel.SetActive(true);
+    }
+
+    private void WordPanelTextClear()
+    {
+        clickedText.text = " ";
+        spinText.text = " ";
     }
 
     IEnumerator playSpinAnimation()
@@ -32,36 +49,66 @@ public class spinWheel : MonoBehaviour
             case 0:
                 wheel.GetComponent<Animator>().Play("wheel_anim_1");
                 yield return new WaitForSeconds(spinAnimation[0].length);
-                audioSource.clip = audios[0];
-                audioSource.Play();
+                // audioSource.clip = audios[0];
+                // audioSource.Play();
+                spinText.text = "et";
+                yield return new WaitForSeconds(1.5f);
+                WordPanelEnable();
+                yield return new WaitForSeconds(1.5f);
+                wordPanel.SetActive(false);
+                WordPanelTextClear();
                 break;
 
             case 1:
                 wheel.GetComponent<Animator>().Play("wheel_anim_2");
                 yield return new WaitForSeconds(spinAnimation[1].length);
-                audioSource.clip = audios[1];
-                audioSource.Play();
+                // audioSource.clip = audios[1];
+                // audioSource.Play();
+                spinText.text = "at";
+                yield return new WaitForSeconds(1.5f);
+                WordPanelEnable();
+                yield return new WaitForSeconds(1.5f);
+                WordPanelTextClear();
+                wordPanel.SetActive(false);
                 break;
 
             case 2:
                 wheel.GetComponent<Animator>().Play("wheel_anim_3");
                 yield return new WaitForSeconds(spinAnimation[2].length);
-                audioSource.clip = audios[2];
-                audioSource.Play();
+                // audioSource.clip = audios[2];
+                // audioSource.Play();
+                spinText.text = "ut";
+                yield return new WaitForSeconds(1.5f);
+                WordPanelEnable();
+                yield return new WaitForSeconds(1.5f);
+                WordPanelTextClear();
+                wordPanel.SetActive(false);
                 break;
 
             case 3:
                 wheel.GetComponent<Animator>().Play("wheel_anim_4");
                 yield return new WaitForSeconds(spinAnimation[3].length);
-                audioSource.clip = audios[3];
-                audioSource.Play();
+                // audioSource.clip = audios[3];
+                // audioSource.Play();
+                spinText.text = "ot";
+                yield return new WaitForSeconds(1.5f);
+                WordPanelEnable();
+                yield return new WaitForSeconds(1.5f);
+                wordPanel.SetActive(false);
+                WordPanelTextClear();
                 break;
 
             case 4:
                 wheel.GetComponent<Animator>().Play("wheel_anim_5");
                 yield return new WaitForSeconds(spinAnimation[4].length);
-                audioSource.clip = audios[4];
-                audioSource.Play();
+                // audioSource.clip = audios[4];
+                // audioSource.Play();
+                spinText.text = "it";
+                yield return new WaitForSeconds(1.5f);
+                WordPanelEnable();
+                yield return new WaitForSeconds(1.5f);
+                wordPanel.SetActive(false);
+                WordPanelTextClear();
                 break;
         }
     }
